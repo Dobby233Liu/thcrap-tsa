0f70371f6d4dc0bdb99be30d1b223706f9960265
Liu Wenyuan <15816141883@163.com>
2025-08-18 19:30:05 +0800

    instant_ending: th20

---
+++ instant_ending/th20.v1.00a.js
@@ -0,0 +1,15 @@
+{
+	"binhacks": {
+		"instant_ending": {
+			"addr": "RxBA4EC"
+		},
+		"zero_lives": {
+			"addr": "RxBDEA8"
+		},
+		// couldn't figure out how to port this
+		/*"deaths_to_continues": {
+			"addr": "Rx5d19b",
+			"code": "a104cd4c0083c001a304cd4c00"
+		}*/
+	}
+}
